e696a2217adb3a1eb7971460ae7b0ab03aa351d8
dev-tyta <hardeykoryar7@gmail.com>
2022-04-15 21:09:22 +0100

    Update url_extractor.py
    url extracted and copied to clip board

--- url_extractor.py
+++ url_extractor.py
@@ -2,6 +2,17 @@ import re
 import pyperclip
 
 text = str(pyperclip.paste())
-url = r"[http|https][://]+"
+url = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+"
 
 match_url = re.findall(url, text)
+print(match_url)
+urls = []
+for ur in match_url:
+    urls.append(ur)
+
+if len(urls) > 0:
+    pyperclip.copy('\n'.join(urls))
+    print('Copied to clipboard:')
+    print('\n'.join(urls))
+else:
+    print('No Url addresses found.')
