427e71a85bea715c8a5a9f5f31044c0a037c2bec
dev-tyta <hardeykoryar7@gmail.com>
2022-04-15 02:19:41 +0100

    improved code

--- regex.py
+++ regex.py
@@ -2,12 +2,13 @@ import re
 import pyperclip
 
 
-txt = pyperclip.paste()
-phone = r"[+234][0-9]{13}"
-mail = r"[a-zA-Z0-9]*@[a-zA-Z]*.[a-zA-Z]*"
-mail_org = r"[a-zA-Z0-9\s]*@[a-zA-Z]*.[a-zA-Z]*.[edu|org|io]"
+txt = str(pyperclip.paste())
+
+phone = r"[+234]" \
+        r"[0-9]{13}"
+mail = r"[a-zA-Z0-9._%+-]+@[a-zA-Z]+.[a-zA-Z]+"
+
 match_phone = re.findall(phone, txt)
 match_mail = re.findall(mail, txt)
-match_org = re.findall(mail_org, txt)
 
-print(f"Phone numbers: {match_phone}, Mail: {match_mail}, School mail : {match_org}")
+print(f"Phone numbers: {match_phone}, Mail: {match_mail}")
