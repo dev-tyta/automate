8f3a368ca8f66dd7f5d0996a3d2434097629d0ab
dev-tyta <hardeykoryar7@gmail.com>
2022-04-18 22:57:25 +0100

    new capital folder added

--- random_quiz.py
+++ random_quiz.py
@@ -1,17 +1,12 @@
-import shelve
+import url
 import pyperclip
 import random
 
-# shelfData = shelve.open("mydata")
-# musicians = ["Khalid", "Wizkid", "James Arthur", "Lewis Capaldi", "Jacob Banks"]
-
-# shelfData["musicians"] = musicians
-# shelfData.close()
-
+capitals = url.capitals
 
 for quizNum in range(35):
-    quiz_file = open(fr"capital_quiz{quizNum + 1}.txt", "w")
-    ans_key = open(fr"capital_quizans{quizNum + 1}.txt", "w")
+    quiz_file = open(fr"C:\Users\Testys\Documents\GitHub\automate\capital\capital_quiz{quizNum + 1}.txt", "w")
+    ans_key = open(fr"C:\Users\Testys\Documents\GitHub\automate\capital\capital_quizans{quizNum + 1}.txt", "w")
 
     quiz_file.write("Name:\n\nDate:\n\nPeriod:\n\n")
     quiz_file.write((" " * 20) + f"State Capitals Quiz(Form{quizNum + 1})")
