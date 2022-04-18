47863e43202a258544e42aaed84332efc12c2776
dev-tyta <hardeykoryar7@gmail.com>
2022-04-19 00:00:07 +0100

    automated quiz generator

--- random_quiz.py
+++ random_quiz.py
@@ -1,5 +1,4 @@
 import url
-import pyperclip
 import random
 
 capitals = url.capitals
@@ -14,3 +13,19 @@ for quizNum in range(35):
 
     states = list(capitals.keys())
     random.shuffle(states)
+
+    for quest_num in range(50):
+        cor_ans = capitals[states[quest_num]]
+        wrong_ans = list(capitals.values())
+        del wrong_ans[wrong_ans.index(cor_ans)]
+        wrong_ans = random.sample(wrong_ans, 3)
+        ans_opt = wrong_ans + [cor_ans]
+
+        quiz_file.write(f"{quest_num + 1}. What is the capital of {states[quest_num]}")
+        for i in range(4):
+            quiz_file.write(f"      {'ABCD'[i]}. {ans_opt[1]}\n")
+        quiz_file.write("\n")
+
+        ans_key.write(f"{quest_num + 1}. {'ABCD'[ans_opt.index(cor_ans)]}")
+quiz_file.close()
+ans_key.close()
