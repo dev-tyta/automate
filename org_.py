fab7d0e9c1f429f30308f9c9653ce7d41a4fe267
dev-tyta <hardeykoryar7@gmail.com>
2022-04-23 21:57:21 +0100

    loads file to compress.

--- org_.py
+++ org_.py
@@ -2,14 +2,19 @@ import zipfile
 import pyinputplus as pyip
 from pathlib import Path
 
-inp_file = pyip.inputFilepath("Input path to file you would like to compress:")
-# Collects Filepath to file to be compressed
 
-path_to_file = Path(inp_file)
+try:
+    inp_file = pyip.inputFilepath("Input path to file you would like to compress:")
+    # Collects Filepath to file to be compressed
 
-new_file = zipfile.ZipFile("file2zip", "w")  # Creates a writeable zipfile
+    path_to_file = Path(inp_file)
 
-new_file.write(path_to_file, compress_type= zipfile.ZIP_DEFLATED)
-new_file.close()
+    new_file = zipfile.ZipFile("file2zip", "w")  # Creates a writeable zipfile
 
-print(fr"Zipped file is located under {Path.cwd()}")
+    new_file.write(path_to_file, compress_type= zipfile.ZIP_DEFLATED)
+    new_file.close()
+
+    print(fr"Zipped file is located under {Path.cwd()}")
+
+except FileNotFoundError:
+    print("File path does not exist")
