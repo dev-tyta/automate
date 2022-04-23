c181f6b77f3824a641adaf0299c7c8116d3e1a6c
dev-tyta <hardeykoryar7@gmail.com>
2022-04-23 18:52:07 +0100

    zip_file info

--- org_.py
+++ org_.py
@@ -2,4 +2,8 @@ import shutil
 import zipfile
 from pathlib import Path
 
-shutil.move(r".\mcb_data\mydata.dat", ".\capital")
+# shutil.move(r".\mcb_data\mydata.dat", r".\capital")
+path = r"C:\Users\Testys\Documents"
+
+example_zip = zipfile.ZipFile(path + r"\exam.zip")
+print(example_zip.namelist())
