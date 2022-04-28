3d88b189ce18ed29bb5e89010f8355255b190f5e
dev-tyta <hardeykoryar7@gmail.com>
2022-04-28 09:34:37 +0100

    traceback text saver

---
+++ traceback_txt.py
@@ -0,0 +1,1 @@
+import tracebacktry:    raise Exception("This is an error message.")except:    error_file = open("errorinfo.txt", "w")    error_file.write(traceback.format_exc())    error_file.close()    print("The traceback info was written to errorInfo.txt")
